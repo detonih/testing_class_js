ec3839efe73c9247463d41feb201343a1bd03c0e
Henrique Detoni <detonihenrique@gmail.com>
2020-04-21 09:43:13 -0300

    add body parser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const { router } = require('./routes/Router')
+const bodyParser = require('body-parser')
 
 class App {
     constructor() {
         this.app = express()
-        this.app.use(express.json())
+        this.middlewares()
+        this.routes()
+    }
+    
+    middlewares() {
+        this.app.use(bodyParser.urlencoded({ extended: false }))
+        this.app.use(bodyParser.json())    
+    }
+
+    routes() {
         this.app.use(router)
     }
+    
 }
 
 module.exports = new App()
